62030bf0837db2c078ac57ca91a9f40f48afd97f
tojonirinaAndrian <andrianjafiniaina21@gmail.com>
2026-07-05 21:24:39 +0300

    components changes : added search bar and some mock people

--- app/presence/page.tsx
+++ app/presence/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useState } from "react";
+import { MagnifyingGlassIcon } from "@phosphor-icons/react";
 
 export default function Presence() {
-  const [userChoiceInFaire, setUserChoiceInFaire] = useState<
-    "photo" | "qrCode" | "liste"
-  >("photo");
+  const [userChoiceInFaire, setUserChoiceInFaire] = useState<"qrCode" | "list">(
+    "qrCode",
+  );
 
   const [userChoice, setUserChoice] = useState<"faire" | "voir">("faire");
   const choosedButtonStyle = "text-black !bg-white";
@@ -26,18 +27,10 @@ export default function Presence() {
           Voir la presence
         </button>
       </div>
-      <div className="flex flex-col p-2 gap-2">
+      <div className="flex flex-col gap-5 p-5">
         {userChoice === "faire" && (
           <>
             <div className="bg-gray-50 flex gap-1 p-2 rounded-xl font-semibold w-fit *:p-3 *:rounded-md text-gray-400">
-              <button
-                onClick={() => setUserChoiceInFaire("photo")}
-                className={
-                  userChoiceInFaire === "photo" ? choosedButtonStyle : ""
-                }
-              >
-                Photo
-              </button>
               <button
                 onClick={() => setUserChoiceInFaire("qrCode")}
                 className={
@@ -47,15 +40,34 @@ export default function Presence() {
                 QR Code
               </button>
               <button
-                onClick={() => setUserChoiceInFaire("liste")}
+                onClick={() => setUserChoiceInFaire("list")}
                 className={
-                  userChoiceInFaire === "liste" ? choosedButtonStyle : ""
+                  userChoiceInFaire === "list" ? choosedButtonStyle : ""
                 }
               >
                 Liste
               </button>
             </div>
-            <div>Faire la presence</div>
+            <div className="w-full p-2 bg-gray-50 rounded-xl flex gap-2">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                className="w-full focus:outline-0 p-2"
+                placeholder="Le nom ou le prenom..."
+              />
+              <button className="hover:bg-gray-200 p-3 rounded-md">
+                <MagnifyingGlassIcon size={32} />
+              </button>
+            </div>
+            <div className="w-full p-2 border border-gray-100 rounded-md flex flex-col gap-1">
+              <div className="w-full p-3 bg-gray-50 rounded-md flex items-center gap-2">
+                <div className="p-5 rounded-full bg-black"></div>
+                <p>
+                  Andrianjafiniaina Tojonirina Nael
+                </p>
+              </div>
+            </div>
           </>
         )}
         {userChoice === "voir" && <div>Voir la presence</div>}
