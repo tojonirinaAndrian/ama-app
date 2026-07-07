bbb1484dff42fc9014ec274554535463e646972d
tojonirinaAndrian <andrianjafiniaina21@gmail.com>
2026-07-07 03:00:59 +0300

    using mock api for users data

--- app/presence/page.tsx
+++ app/presence/page.tsx
@@ -1,19 +1,33 @@
 "use client";
-import { useState } from "react";
-import { MagnifyingGlassIcon } from "@phosphor-icons/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Presence() {
-  const [userChoiceInFaire, setUserChoiceInFaire] = useState<"qrCode" | "list">(
-    "qrCode",
-  );
+  const [users, setUsers] = useState([]);
 
-  const [userChoice, setUserChoice] = useState<"faire" | "voir">("faire");
-  const choosedButtonStyle = "text-black !bg-white";
-  const choosedActionButtonStyle = "!text-black !border-b-black";
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/users');
+        console.log('Fetched users:', response.data.users);
+        setUsers(response.data.users);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h2 className="font-semibold text-3xl">Présence</h2>
+      <div>
+        {users.map((user: any) => (
+          <div key={user.id}>
+            <p>{user.firstName} {user.lastName}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
