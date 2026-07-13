1d161e097dcef942e9fb47c18a3b797210f3a764
tojonirinaAndrian <andrianjafiniaina21@gmail.com>
2026-07-14 01:51:47 +0300

    tried adding ctrl+k feature but not working yet

--- app/components/presence/searchComponent.tsx
+++ app/components/presence/searchComponent.tsx
@@ -13,16 +13,24 @@ export default function SearchComponent() {
         searchElement?.addEventListener("focusout", () => {
             setIsInputting(false)
         });
+        window.addEventListener("keypress", (e) => {
+            // console.log(e.coe)
+            if (e.code === "KeyK" && e.ctrlKey) {
+                e.preventDefault();
+                console.log(e.code);
+            }}
+        )
     }, []);
     const onCancelClick = () => {
         const searchElement: HTMLInputElement = document.getElementById("searchInput") as HTMLInputElement;
         searchElement.value = "";
+        searchElement.focus();
         setUserInput("");
     };
     return <>
         <div className="p-5 flex flex-col">
             <div
-                className={`xl:w-[45%] relative border flex gap-1 p-1 items-center rounded border-gray-200 bg-gray-50 ${isInputting && "outline outline-gray-500 bg-white"}`}>
+                className={`xl:w-[45%] md:w-[60%] relative border flex gap-1 p-1 items-center rounded border-gray-200 bg-gray-50 ${isInputting && "outline outline-gray-500 bg-white"}`}>
 
                 <div className="p-2.5 pr-1 cursor-text"
                     onClick={() => {
@@ -32,21 +40,23 @@ export default function SearchComponent() {
                 >
                     <MagnifyingGlassIcon className="text-gray-500" size={24} />
                 </div>
-                <input type="text" className={` h-full w-full focus:outline-none ${!isInputting ? "text-gray-800" : "text-black"}`}
+                <input type="text" className={`z-1 h-full w-full focus:outline-none ${!isInputting ? "text-gray-800" : "text-black"}`}
                     id="searchInput"
                     placeholder="Rechercher un membre..."
                     onChange={(e) => {
                         setUserInput(e.target.value)
                     }}
                 />
-                {(userInput.length >= 1) && <button
+                {(userInput.length >= 1) ? <button
                     onClick={onCancelClick}
                     className="cursor-pointer w-fit flex gap-2 px-3 items-center bg-white border rounded p-2 border-gray-300">
                     <BackspaceIcon size={18} />
                     <span className="">
                         Annuler
                     </span>
-                </button>}
+                </button> : (!isInputting) && <span className="absolute right-4 text-gray-400">
+                    ctrl+K
+                </span>}
             </div>
             <p>{userInput}</p>
         </div>
