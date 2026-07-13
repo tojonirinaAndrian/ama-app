7820bb0f618e0f0b88843abae829a29b22c8cfdd
tojonirinaAndrian <andrianjafiniaina21@gmail.com>
2026-07-14 01:02:42 +0300

    set up search Component styles to behave like a real search input

--- app/components/presence/searchComponent.tsx
+++ app/components/presence/searchComponent.tsx
@@ -1,10 +1,11 @@
 'use client';
-import { MagnifyingGlassIcon } from "@phosphor-icons/react";
+import { MagnifyingGlassIcon, BackspaceIcon } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 
 export default function SearchComponent() {
     const [isInputting, setIsInputting] = useState<boolean>(false);
-    useEffect (() => {
+    const [userInput, setUserInput] = useState<string>("");
+    useEffect(() => {
         const searchElement = document.getElementById("searchInput");
         searchElement?.addEventListener("focusin", () => {
             setIsInputting(true)
@@ -12,20 +13,43 @@ export default function SearchComponent() {
         searchElement?.addEventListener("focusout", () => {
             setIsInputting(false)
         });
-    }, [])
+    }, []);
+    const onCancelClick = () => {
+        const searchElement: HTMLInputElement = document.getElementById("searchInput") as HTMLInputElement;
+        searchElement.value = "";
+        setUserInput("");
+    };
     return <>
-        <div className="p-3 flex flex-col">
-            <div 
-            className={`border flex gap-2 items-center p-3 rounded border-gray-200 bg-gray-50 ${isInputting && "outline outline-gray-500 bg-white"}`}>
-                <MagnifyingGlassIcon className="text-gray-500"/>
-                <input type="text" className="w-full focus:outline-none"
+        <div className="p-5 flex flex-col">
+            <div
+                className={`relative border flex gap-1 p-1 items-center rounded border-gray-200 bg-gray-50 ${isInputting && "outline outline-gray-500 bg-white"}`}>
+
+                <div className="p-2.5 pr-1 cursor-text"
+                    onClick={() => {
+                        const searchElement: HTMLInputElement = document.getElementById("searchInput") as HTMLInputElement;
+                        searchElement.focus();
+                    }}
+                >
+                    <MagnifyingGlassIcon className="text-gray-500" size={24} />
+
+                </div>
+                <input type="text" className={` h-full w-full focus:outline-none ${!isInputting ? "text-gray-800" : "text-black"}`}
                     id="searchInput"
                     placeholder="Rechercher un membre..."
-                    // onKeyUp={(e) => {
-                    //     console.log(e.detail)
-                    // }}
+                    onChange={(e) => {
+                        setUserInput(e.target.value)
+                    }}
                 />
+                {(userInput.length >= 1) && <button
+                    onClick={onCancelClick}
+                    className="cursor-pointer w-fit flex gap-2 px-3 items-center bg-white border rounded p-2 border-gray-300">
+                    <BackspaceIcon size={18} />
+                    <span className="">
+                        Annuler
+                    </span>
+                </button>}
             </div>
+            <p>{userInput}</p>
         </div>
     </>
 }
