01f95da5ae38c760498461a93df2d2e0656cc8e8
tojonirinaAndrian <andrianjafiniaina21@gmail.com>
2026-07-13 23:28:06 +0300

    made the search animation work for the whole component but not only in the input using native getElementById

--- app/components/presence/searchComponent.tsx
+++ app/components/presence/searchComponent.tsx
@@ -1,14 +1,31 @@
+'use client';
 import { MagnifyingGlassIcon } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
 
-export default function  () {
+export default function SearchComponent() {
+    const [isInputting, setIsInputting] = useState<boolean>(false);
+    useEffect (() => {
+        const searchElement = document.getElementById("searchInput");
+        searchElement?.addEventListener("focusin", () => {
+            setIsInputting(true)
+        });
+        searchElement?.addEventListener("focusout", () => {
+            setIsInputting(false)
+        });
+    }, [])
     return <>
-    <div className="p-3 flex flex-col">
-        <div className="border flex gap-2 items-center p-3 rounded border-gray-200 bg-gray-50">
-        <MagnifyingGlassIcon />
-        <input type="text" className="w-full" placeholder="
-        Rechercher un membre..."/>
-        
+        <div className="p-3 flex flex-col">
+            <div 
+            className={`border flex gap-2 items-center p-3 rounded border-gray-200 bg-gray-50 ${isInputting && "outline outline-gray-500 bg-white"}`}>
+                <MagnifyingGlassIcon className="text-gray-500"/>
+                <input type="text" className="w-full focus:outline-none"
+                    id="searchInput"
+                    placeholder="Rechercher un membre..."
+                    // onKeyUp={(e) => {
+                    //     console.log(e.detail)
+                    // }}
+                />
+            </div>
         </div>
-    </div>
     </>
 }
